b9d3683d74eebf9dd0bd3d57b02f8573d8b69df0
Pasquale Muraca <pasqualemuraca2003@gmail.com>
2024-07-04 21:39:39 +0200

    Header anti-clickjacking

---
+++ FratelliMuraca/src/filter/ClickjackFilter.java
@@ -0,0 +1,50 @@
+package filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet Filter implementation class ClickjackFilter
+ */
+@WebFilter("/ClickjackFilter")
+public class ClickjackFilter implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public ClickjackFilter() {
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// place your code here
+		if (response instanceof HttpServletResponse) {
+            HttpServletResponse httpResponse = (HttpServletResponse) response;
+            httpResponse.setHeader("X-Frame-Options", "SAMEORIGIN");
+        }
+
+		chain.doFilter(request, response);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+	}
+
+}
